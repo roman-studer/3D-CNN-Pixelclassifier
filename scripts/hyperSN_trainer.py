6c215e5d02be1e7952001671edb5f2c103b10b61
Roman Studer <studer.r@outlook.com>
2024-02-16 16:00:13 +0100

    Update hyperSN_trainer.py

--- scripts/hyperSN_trainer.py
+++ scripts/hyperSN_trainer.py
@@ -11,7 +11,10 @@ print(os.getcwd())
 torch.set_float32_matmul_precision("medium")
 
 # load config from configurations folder (yaml)
-config = yaml.safe_load(open("../configurations/hyperSN_config.yaml"))
+# TODO path to config file changes between local and server
+config = yaml.safe_load(
+    open("./3D-CNN-Pixelclassifier/configurations/hyperSN_config.yaml")
+)
 
 config_hyperSN = config["hyperSN"]
 config_dataloader = config["hyperSN_dataloader"]
